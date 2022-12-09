e99b99ebc072b294124189a2f6615619b1ad3a60
zaynabkrm <zaynabelkaroumi@gmail.com>
2022-12-09 12:04:28 -0800

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /*
- * main - Entry  
- * desciption My first code using c language 
+ * main - print a line of code using puts
+ *
  * Return: 0
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
