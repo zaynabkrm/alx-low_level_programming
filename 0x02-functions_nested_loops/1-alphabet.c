f368f46d2165e361b0493a150753b50ce6a95232
zaynabkrm <zaynabelkaroumi@gmail.com>
2022-12-13 14:29:00 -0800

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - print Alphabet
+ *
+ * Return: 0
+ */
+void print_alphabet(void);
+{
+	char al;
+	
+	for (al = 'a'; al <= 'z'; al++)
+		_putchar(al);
+	_putchar('\n');
+}
