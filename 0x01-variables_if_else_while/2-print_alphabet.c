15f2d670b8099b59c124f5bdb149834a595738bb
zaynabkrm <zaynabelkaroumi@gmail.com>
2022-12-13 18:32:10 -0800

    Alphabet 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,14 +7,15 @@
  *
  * Return: 0
  */
-int main()
+int main(void)
 {
 	char al;
 
-	for (al = "a"; al <= "z"; al++)
+	for (al = 'a'; al <= 'z'; al++)
 	{
 		putchar(al);
 	}
-	putchar("\n");
+	putchar('\n');
+
 	return (0);
 }
