a6359d8862f5b4877b1f965aa0f842248b26c9d5
zaynabkrm <zaynabelkaroumi@gmail.com>
2022-12-19 15:44:31 -0800

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - printa string to the stdout
+ * @str: the string
+ * Return: Length of String
+ */
+
+void _puts(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		_putchar(str[i]);
+		a++;
+	}
+	_putchar('\n');
+}
