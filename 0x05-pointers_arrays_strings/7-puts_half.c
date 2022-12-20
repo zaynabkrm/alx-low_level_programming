cd0e3cde6ae5c0baa214ba315088c08478df79be
zaynabkrm <zaynabelkaroumi@gmail.com>
2022-12-19 16:36:48 -0800

    Task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,14 +8,14 @@ void puts_half(char *str)
 {
 	int len = 0;
 
-	while (*str != '\n')
+	while (*str != '\0')
 	{
 		len++;
 		str++;
 	}
 
 	str -= (len / 2);
-	while (*str != '\n')
+	while (*str != '\0')
 	{
 		_putchar(*str);
 		str++;
