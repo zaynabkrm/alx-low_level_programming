f282f1fc1b45178d12bc9c7424d200c37df93980
zaynabkrm <zaynabelkaroumi@gmail.com>
2022-12-19 16:26:56 -0800

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - print 1 character out of 2
+ * @str: input
+ * Return: print
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	for (; str[i] != '\0'; i++)
+	{
+		if ((i % 2) == 0)
+			_putchar(str[i]);
+		else
+			continue;
+	}
+	_putchar('\n');
+}
