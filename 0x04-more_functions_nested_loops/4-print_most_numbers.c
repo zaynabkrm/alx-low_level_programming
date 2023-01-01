bb87890503d492c5716c05c04869e77511076d36
zaynabkrm <zaynabelkaroumi@gmail.com>
2023-01-01 14:07:30 -0800

    Task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,12 +8,16 @@
  */
 void print_most_numbers(void)
 {
-	int i;
+	int i = 0;
 
-	while (i < 10 && i != 4 && i != 2)
+	while (i < 10)
 	{
+		if ((i ==2) || (i ==4))
+		{
+			break;
+		}
 		i++;
-		putchar(i + '0');
+		_putchar(i);
 	}
 
 putchar('\n');
