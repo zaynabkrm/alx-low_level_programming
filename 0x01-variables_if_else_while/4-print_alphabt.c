8200dbcf8209e72bfa148cf6060dccd74f372835
zaynabkrm <zaynabelkaroumi@gmail.com>
2023-01-01 04:11:46 -0800

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,15 +7,15 @@
  */
 int main(void)
 {
-	char c;
+char c;
 
-	c = 'a';
-	while
-	       (c <= 'z') {
-		       if ((c != 'q') && (c != 'e') && (c <= 'z'))
-			       putchar(c);
-		       c++;
-	       }
-	putchar('\n');
-	return (0);
+c = 'a';
+while
+(c <= 'z') {
+if ((c != 'q') && (c != 'e') && (c <= 'z'))
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
 }
