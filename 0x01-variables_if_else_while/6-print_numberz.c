1ea2ec302b711ece7bc89424fb703174fee850cd
zaynabkrm <zaynabelkaroumi@gmail.com>
2023-01-01 05:04:38 -0800

    TAsk 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Print single digit
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int a;
+a = 0;
+while
+(a < 10)
+{
+putchar('a +'0');
+a++;
+}
+putchar('\n');
+return (0);
+}
