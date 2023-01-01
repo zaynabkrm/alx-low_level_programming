751a0178ce194ae891dcb2c865acf792cf1fe167
zaynabkrm <zaynabelkaroumi@gmail.com>
2023-01-01 09:08:52 -0800

    Task 11

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main -  prints combinations of two digit
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+int j;
+for (i = 0; i <= 9 ; i++)
+{
+for (j = 0; j <= 9 ; j++)
+{
+putchar(i + '0');
+putchar(j + '0');
+if (i < 8)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
