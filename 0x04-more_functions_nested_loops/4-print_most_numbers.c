3c9fffc96958c539ef737c28e9671c195d43f9b7
zaynabkrm <zaynabelkaroumi@gmail.com>
2023-01-01 14:41:16 -0800

    Task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,21 +4,19 @@
 /**
  * print_most_numbers - prints the numbers, from 0 to 9 -{2,4}
  *
- * Return:0
+ * Return: nothing
  */
 void print_most_numbers(void)
 {
-	int i = 0;
-
-	while (i < 10)
+	int i;
+	for (i = 0; i < 10; i++)
 	{
 		if ((i == 2) || (i == 4))
 		{
 			continue;
 		}
-		putchar(i);
-                i++;
+		_putchar(i);
 	}
-	putchar('\n');
+	_putchar(10);
 }
 
