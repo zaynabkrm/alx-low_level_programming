8703a977cae63918dbb5125dd5d369abf62b5241
zaynabkrm <zaynabelkaroumi@gmail.com>
2023-01-01 09:21:19 -0800

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,21 +6,22 @@
  */
 int main(void)
 {
-int i;
-int j;
-for (i = 0; i <= 9 ; i++)
-{
-for (j = 0; j <= 9 ; j++)
-{
-putchar(i + '0');
-putchar(j + '0');
-if (i < 8)
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-putchar('\n');
-return (0);
+	int i;
+	int j;
+
+	for (i = 0; i <= 9 ; i++)
+	{
+		for (j = i + 1; j <= 9 ; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if (i < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
