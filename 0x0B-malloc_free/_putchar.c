e69c17986ad241418fb70a1ddf5c5676621c30ac
zaynabkrm <zaynabelkaroumi@gmail.com>
2023-01-10 12:22:01 -0800

    files

---
+++ 0x0B-malloc_free/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the char to print
+ * Return: on succes 1.
+ * on error, -1 is returned, and error is set approriately
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
