f2d2daeea76caa502b8b0c09ae15bf1e75f9d70b
zaynabkrm <zaynabelkaroumi@gmail.com>
2023-09-30 10:48:49 -0700

    task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,11 +3,13 @@
 /**
  * _strlen_recursion -returns the length of a string.
  * @s: String
+ * description
  * return: int
  */
 int _strlen_recursion(char *s)
-	int len = 0;
-	
+{
+int len = 0;
+
 	if (*s > '\0')
 {
 	len += _strlen_recursion(s + 1) + 1;
