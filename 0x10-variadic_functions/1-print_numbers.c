6eeeb5a01216b931e4819df88a186f03d8686eaf
zaynabkrm <zaynabelkaroumi@gmail.com>
2023-10-24 08:33:17 -0700

    1-print_numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -8,7 +8,7 @@
  *
  * Return: void
  */
-void print_numbers(const char *separator, const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i = n;
 	va_list ap;
