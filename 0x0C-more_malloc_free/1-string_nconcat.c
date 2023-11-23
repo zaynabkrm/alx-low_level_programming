57af898383fe825809cb653a7ab73663a3cc7d62
zaynabkrm <zaynabelkaroumi@gmail.com>
2023-11-23 13:07:42 -0800

    allocates memory for an array

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * char *string_nconcat -concatenates two strings
+ * @s1: pointer to first string
+ * @s2: pointer to second string
+ * @n: number of bytes to concatenate
+ *
+ * Return: pointer to space in memory
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+{
+
+
+}
