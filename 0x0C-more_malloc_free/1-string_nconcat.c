ca896cdeacc2dc175c5e2f6f2da953aca375fbe0
zaynabkrm <zaynabelkaroumi@gmail.com>
2023-11-23 13:41:42 -0800

    concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  * char *string_nconcat -concatenates two strings
@@ -8,9 +9,37 @@
  *
  * Return: pointer to space in memory
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 {
+        char *str
+	unsigned int i, j, len1, len2;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+
+	if (n < len2)
+		str = malloc(sizeof(char) * (len1 + n + 1));
+	else
+		str = malloc(sizeof(char) * (len1 + len2 + 1));
+
+	if (!str)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < len1; i++)
+	{
+		str[i] = s1[i];
+	}
+	for (j = 0; j < n && s2[j] != '\0'; j++)
+	{
+		str[i++] = s2[j];
+	}
+	str[i] = '\0';
+
+	return (str);
 }
