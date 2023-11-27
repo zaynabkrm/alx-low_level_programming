d97aa0fd4748b2a4b7d80f33a5f2760a16ab911c
zaynabkrm <zaynabelkaroumi@gmail.com>
2023-11-27 15:02:03 -0800

    free dog

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,3 +1,14 @@
 #include <stdio.h>
 #include "dog.h"
-
+/**
+ * free_dog - frees dogs
+ * @d: the dog to free
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
