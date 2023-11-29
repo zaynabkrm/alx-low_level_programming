9f6d743e57d2a64e7f2d4816f7fe49bd341465a2
zaynabkrm <zaynabelkaroumi@gmail.com>
2023-11-29 07:55:15 -0800

    prints a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts_recursion - prints a string, followed by a new line.
+ * @s: string
+ * return: 0
+ */
+
+void _puts_recursion(char *s)
+{
+if (*s == '\0')
+{
+	_putchar('\n');
+}
+else
+{
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
+}
