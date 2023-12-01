e7c116cb185176e6b691c882afed9ea8deb54bc3
zaynabkrm <zaynabelkaroumi@gmail.com>
2023-11-30 16:16:29 -0800

    compares two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,9 +7,9 @@
  * @s2: second string
  * Return: number
  */
-int _strcmp(char *s1, char *s2);
+int _strcmp(char *s1, char *s2)
 {
-	while (*s1 && (*s1 ==*s2))
+	while (*s1 && (*s1 ==* s2))
 	{
 		s1++;
 		s2++;
